b205f5848fb062641c4f93cedbeaad77f97fa4e9
Divyansh Rathore <rathoredebu10@gamil.com>
2025-11-05 14:43:53 +0530

    login and signup basics complete

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -12,5 +12,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT} http://localhost:${PORT}`));
